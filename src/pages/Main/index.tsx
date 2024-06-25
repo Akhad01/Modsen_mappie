7ae9f358e322a22b7d5815008433f35841bf7b3f
Akhad <abdurazakovahad@gmail.com>
2024-06-25 14:39:26 +0300

    added centering and radius for the label

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,19 +1,62 @@
-import { Map, Placemark, YMaps } from '@pbe/react-yandex-maps';
+import { Circle, Map, Placemark, YMaps } from '@pbe/react-yandex-maps';
 
 import Sidebar from '../../components/Sidebar';
 import { Container } from './styled';
+import { mark } from '../../assets/icons';
+import { useEffect, useState } from 'react';
+
+console.log('mark', mark);
 
 const Main = () => {
+  const [userLocation, setUserLocation] = useState<[number, number] | null>(
+    null
+  );
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation([latitude, longitude]);
+        },
+        (error) => {
+          console.error('Error getting location', error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  }, []);
+
+  const defaultState = {
+    center: userLocation || [55.751244, 37.618423], // Центр карты (Москва) по умолчанию
+    zoom: userLocation ? 12 : 10,
+  };
+
+  console.log('userLocation', userLocation);
+
   return (
     <YMaps>
       <Container>
         <Sidebar />
-        <Map
-          height='100vh'
-          width='100%'
-          defaultState={{ center: [55.75, 37.57], zoom: 9 }}
-        >
-          <Placemark defaultGeometry={[55.751574, 37.573856]} />
+        <Map height='100vh' width='100%' defaultState={defaultState}>
+          {userLocation && (
+            <Placemark
+              options={{
+                iconLayout: 'default#image',
+                iconImageHref: mark,
+                iconImageSize: [32, 32],
+              }}
+              geometry={userLocation}
+            />
+          )}
+          <Circle
+            geometry={[userLocation, 10000]}
+            options={{
+              fillColor: '#5E7BC7',
+              fillOpacity: 0.5,
+            }}
+          />
         </Map>
       </Container>
     </YMaps>
