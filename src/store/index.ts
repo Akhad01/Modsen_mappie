e0b522199d43b697a9769ea5cee26e467e62e3c0
Akhad <abdurazakovahad@gmail.com>
2024-06-27 14:24:22 +0300

    added user-slices

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sidebarReducer from './slices/sidebar-slice';
+import userReducer from './slices/user-slices';
 
 export const store = configureStore({
   reducer: {
     sidebar: sidebarReducer,
+    user: userReducer,
   },
 });
 
